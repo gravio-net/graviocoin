6d7deaf96e7b3bb352e7fe8339ece1eafa11174e
szhelonkin <zhelonkin@gravio.net>
2017-07-27 11:38:43 +0300

    Fixed "Fusion" style, wich used for wimdows

--- src/qt/platformstyle.cpp
+++ src/qt/platformstyle.cpp
@@ -23,9 +23,9 @@ static const struct {
     const bool useExtraSpacing;
 } platform_styles[] = {
     {"macosx", false, false, true},
-    {"windows", true, false, false},
+    {"windows", true, false, false}, /*false*/
     /* Other: linux, unix, ... */
-    {"other", true, true, false}
+    {"other", true, true, false} /*false*/
 };
 static const unsigned platform_styles_count = sizeof(platform_styles)/sizeof(*platform_styles);
 
